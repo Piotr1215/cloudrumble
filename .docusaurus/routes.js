c3a825b658e1f0451de8075804c210975eff7df8
decoder <decoder@live.de>
2022-02-10 23:23:18 +0100

    root dir

---
+++ .docusaurus/routes.js
@@ -0,0 +1,472 @@
+
+import React from 'react';
+import ComponentCreator from '@docusaurus/ComponentCreator';
+
+export default [
+  {
+    path: '/__docusaurus/debug',
+    component: ComponentCreator('/__docusaurus/debug','3d6'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/config',
+    component: ComponentCreator('/__docusaurus/debug/config','914'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/content',
+    component: ComponentCreator('/__docusaurus/debug/content','c28'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/globalData',
+    component: ComponentCreator('/__docusaurus/debug/globalData','3cf'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/metadata',
+    component: ComponentCreator('/__docusaurus/debug/metadata','31b'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/registry',
+    component: ComponentCreator('/__docusaurus/debug/registry','0da'),
+    exact: true
+  },
+  {
+    path: '/__docusaurus/debug/routes',
+    component: ComponentCreator('/__docusaurus/debug/routes','244'),
+    exact: true
+  },
+  {
+    path: '/blog',
+    component: ComponentCreator('/blog','455'),
+    exact: true
+  },
+  {
+    path: '/blog/5-common-pitfalls-in-iac',
+    component: ComponentCreator('/blog/5-common-pitfalls-in-iac','9fd'),
+    exact: true
+  },
+  {
+    path: '/blog/5-static-websites-deployment-options',
+    component: ComponentCreator('/blog/5-static-websites-deployment-options','da6'),
+    exact: true
+  },
+  {
+    path: '/blog/5-unusual-docker-usecases',
+    component: ComponentCreator('/blog/5-unusual-docker-usecases','2f1'),
+    exact: true
+  },
+  {
+    path: '/blog/8-tips-to-prepare-for-az-300-exam',
+    component: ComponentCreator('/blog/8-tips-to-prepare-for-az-300-exam','411'),
+    exact: true
+  },
+  {
+    path: '/blog/8-tips-to-prepare-for-az-301-exam',
+    component: ComponentCreator('/blog/8-tips-to-prepare-for-az-301-exam','56a'),
+    exact: true
+  },
+  {
+    path: '/blog/a-gentle-introduction-to-docker-and-containers',
+    component: ComponentCreator('/blog/a-gentle-introduction-to-docker-and-containers','d67'),
+    exact: true
+  },
+  {
+    path: '/blog/archive',
+    component: ComponentCreator('/blog/archive','f4c'),
+    exact: true
+  },
+  {
+    path: '/blog/cloud-native-developer-workflow',
+    component: ComponentCreator('/blog/cloud-native-developer-workflow','9a6'),
+    exact: true
+  },
+  {
+    path: '/blog/expose-kubernetes-service-using-ngrok',
+    component: ComponentCreator('/blog/expose-kubernetes-service-using-ngrok','6e8'),
+    exact: true
+  },
+  {
+    path: '/blog/gitops-with-kubernetes',
+    component: ComponentCreator('/blog/gitops-with-kubernetes','a22'),
+    exact: true
+  },
+  {
+    path: '/blog/how-to-create-kubernetes-homelab',
+    component: ComponentCreator('/blog/how-to-create-kubernetes-homelab','82b'),
+    exact: true
+  },
+  {
+    path: '/blog/how-to-good-sofware-documentatiion',
+    component: ComponentCreator('/blog/how-to-good-sofware-documentatiion','6ec'),
+    exact: true
+  },
+  {
+    path: '/blog/how-to-learn-devops',
+    component: ComponentCreator('/blog/how-to-learn-devops','3df'),
+    exact: true
+  },
+  {
+    path: '/blog/intro-to-azure-ACI',
+    component: ComponentCreator('/blog/intro-to-azure-ACI','472'),
+    exact: true
+  },
+  {
+    path: '/blog/mdx-blog-post',
+    component: ComponentCreator('/blog/mdx-blog-post','bee'),
+    exact: true
+  },
+  {
+    path: '/blog/page/2',
+    component: ComponentCreator('/blog/page/2','cec'),
+    exact: true
+  },
+  {
+    path: '/blog/page/3',
+    component: ComponentCreator('/blog/page/3','c1c'),
+    exact: true
+  },
+  {
+    path: '/blog/portable-kubernetes-management-with-kubectl-in-docker-container',
+    component: ComponentCreator('/blog/portable-kubernetes-management-with-kubectl-in-docker-container','ed0'),
+    exact: true
+  },
+  {
+    path: '/blog/preparation-and-resources-for-CKA-exam',
+    component: ComponentCreator('/blog/preparation-and-resources-for-CKA-exam','f0b'),
+    exact: true
+  },
+  {
+    path: '/blog/preparation-and-resources-for-CKAD-exam',
+    component: ComponentCreator('/blog/preparation-and-resources-for-CKAD-exam','f33'),
+    exact: true
+  },
+  {
+    path: '/blog/software-development-in-kubernetes-a-cookbook',
+    component: ComponentCreator('/blog/software-development-in-kubernetes-a-cookbook','cbe'),
+    exact: true
+  },
+  {
+    path: '/blog/tags',
+    component: ComponentCreator('/blog/tags','e13'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/architecture',
+    component: ComponentCreator('/blog/tags/architecture','b5f'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/automation',
+    component: ComponentCreator('/blog/tags/automation','216'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/azure',
+    component: ComponentCreator('/blog/tags/azure','122'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/certification',
+    component: ComponentCreator('/blog/tags/certification','04a'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/cli',
+    component: ComponentCreator('/blog/tags/cli','fc3'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/container',
+    component: ComponentCreator('/blog/tags/container','304'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/containers',
+    component: ComponentCreator('/blog/tags/containers','045'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/deployment',
+    component: ComponentCreator('/blog/tags/deployment','3ef'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/design',
+    component: ComponentCreator('/blog/tags/design','53b'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/devops',
+    component: ComponentCreator('/blog/tags/devops','905'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/docker',
+    component: ComponentCreator('/blog/tags/docker','f93'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/documentation',
+    component: ComponentCreator('/blog/tags/documentation','2c3'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/docusaurus',
+    component: ComponentCreator('/blog/tags/docusaurus','075'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/facebook',
+    component: ComponentCreator('/blog/tags/facebook','ede'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/gitops',
+    component: ComponentCreator('/blog/tags/gitops','0c9'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/guide',
+    component: ComponentCreator('/blog/tags/guide','217'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/hello',
+    component: ComponentCreator('/blog/tags/hello','51f'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/homelab',
+    component: ComponentCreator('/blog/tags/homelab','ed3'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/iac',
+    component: ComponentCreator('/blog/tags/iac','23d'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/kubernetes',
+    component: ComponentCreator('/blog/tags/kubernetes','fdb'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/learning',
+    component: ComponentCreator('/blog/tags/learning','767'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/linux',
+    component: ComponentCreator('/blog/tags/linux','6bf'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/ngrok',
+    component: ComponentCreator('/blog/tags/ngrok','dba'),
+    exact: true
+  },
+  {
+    path: '/blog/tags/terraform',
+    component: ComponentCreator('/blog/tags/terraform','ea7'),
+    exact: true
+  },
+  {
+    path: '/blog/the-fundamentals-of-microservices-oriented-architecture',
+    component: ComponentCreator('/blog/the-fundamentals-of-microservices-oriented-architecture','28a'),
+    exact: true
+  },
+  {
+    path: '/blog/try-kubernetes-in-cloud-for-free',
+    component: ComponentCreator('/blog/try-kubernetes-in-cloud-for-free','3f0'),
+    exact: true
+  },
+  {
+    path: '/blog/welcome',
+    component: ComponentCreator('/blog/welcome','bfa'),
+    exact: true
+  },
+  {
+    path: '/impressum',
+    component: ComponentCreator('/impressum','e2a'),
+    exact: true
+  },
+  {
+    path: '/markdown-page',
+    component: ComponentCreator('/markdown-page','be1'),
+    exact: true
+  },
+  {
+    path: '/docs',
+    component: ComponentCreator('/docs','21d'),
+    routes: [
+      {
+        path: '/docs/CERTIFICATES/aws_saa_co2',
+        component: ComponentCreator('/docs/CERTIFICATES/aws_saa_co2','eef'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/CERTIFICATES/az_303_guide',
+        component: ComponentCreator('/docs/CERTIFICATES/az_303_guide','fe7'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/CERTIFICATES/az_304_guide',
+        component: ComponentCreator('/docs/CERTIFICATES/az_304_guide','351'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/CERTIFICATES/cka_guide',
+        component: ComponentCreator('/docs/CERTIFICATES/cka_guide','470'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/CERTIFICATES/ckad_guide',
+        component: ComponentCreator('/docs/CERTIFICATES/ckad_guide','fa1'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/CERTIFICATES/cks_guide',
+        component: ComponentCreator('/docs/CERTIFICATES/cks_guide','976'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/CERTIFICATES/dca_guide',
+        component: ComponentCreator('/docs/CERTIFICATES/dca_guide','cfe'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/CERTIFICATES/gcp_pca',
+        component: ComponentCreator('/docs/CERTIFICATES/gcp_pca','517'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/CERTIFICATES/terraform',
+        component: ComponentCreator('/docs/CERTIFICATES/terraform','15f'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/cheatsheets',
+        component: ComponentCreator('/docs/cheatsheets','1cd'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/coming_soon',
+        component: ComponentCreator('/docs/coming_soon','aea'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/docs',
+        component: ComponentCreator('/docs/docs','650'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/EXAMPLES/',
+        component: ComponentCreator('/docs/EXAMPLES/','e28'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/EXAMPLES/k8s-configuration',
+        component: ComponentCreator('/docs/EXAMPLES/k8s-configuration','006'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/EXAMPLES/k8s-deployments',
+        component: ComponentCreator('/docs/EXAMPLES/k8s-deployments','beb'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/EXAMPLES/k8s-jobs-cronjobs',
+        component: ComponentCreator('/docs/EXAMPLES/k8s-jobs-cronjobs','b3c'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/EXAMPLES/k8s-services',
+        component: ComponentCreator('/docs/EXAMPLES/k8s-services','a33'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/EXAMPLES/k8s-storage-pv-pvc',
+        component: ComponentCreator('/docs/EXAMPLES/k8s-storage-pv-pvc','802'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/faq',
+        component: ComponentCreator('/docs/faq','e79'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/FUNDAMENTALS/Containers/',
+        component: ComponentCreator('/docs/FUNDAMENTALS/Containers/','a2f'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/FUNDAMENTALS/Containers/docker_intro',
+        component: ComponentCreator('/docs/FUNDAMENTALS/Containers/docker_intro','9ce'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/FUNDAMENTALS/GitOps/gitops_intro',
+        component: ComponentCreator('/docs/FUNDAMENTALS/GitOps/gitops_intro','948'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/FUNDAMENTALS/Kubernetes/',
+        component: ComponentCreator('/docs/FUNDAMENTALS/Kubernetes/','5d2'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/FUNDAMENTALS/Kubernetes/kubernetes_intro',
+        component: ComponentCreator('/docs/FUNDAMENTALS/Kubernetes/kubernetes_intro','0b7'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/intro',
+        component: ComponentCreator('/docs/intro','aed'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      },
+      {
+        path: '/docs/resources',
+        component: ComponentCreator('/docs/resources','8be'),
+        exact: true,
+        'sidebar': "tutorialSidebar"
+      }
+    ]
+  },
+  {
+    path: '/',
+    component: ComponentCreator('/','deb'),
+    exact: true
+  },
+  {
+    path: '*',
+    component: ComponentCreator('*')
+  }
+];
