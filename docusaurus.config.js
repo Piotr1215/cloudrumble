6fffae08dfff0e345e056fc9ec4a8d5c53027666
Piotr Zaniewski <piotr@upbound.io>
2022-09-13 19:31:05 +0200

    Adding algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,140 +1,165 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Cloud Rumble',
+  title: "Cloud Rumble",
   tagline:
-      'IT Certifications learning notes and blogs. Kubernetes and cloud native ramblings',
-  url: 'https://frosty-babbage-3125a3.netlify.app',
+    "IT Certifications learning notes and blogs. Kubernetes and cloud native ramblings",
+  url: "https://frosty-babbage-3125a3.netlify.app",
   // url: 'https://www.itcertificationsguide.com/',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.svg',
-  organizationName: 'Piotr1215',  // Usually your GitHub org/user name.
-  projectName: 'dca-prep-kit',    // Usually your repo name.
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.svg",
+  organizationName: "Piotr1215", // Usually your GitHub org/user name.
+  projectName: "dca-prep-kit", // Usually your repo name.
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-            editUrl: ({docPath}) =>
-                        `https://github.com/Piotr1215/dca-prep-kit/edit/master/docs/${docPath}`,
+          editUrl: ({ docPath }) =>
+            `https://github.com/Piotr1215/dca-prep-kit/edit/master/docs/${docPath}`,
         },
         blog: {
           showReadingTime: true,
-          blogSidebarTitle: 'All posts',
-          blogSidebarCount: 'ALL',
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
           postsPerPage: 10,
           // Please change this to your repo.
-          editUrl: 'https://github.com/Piotr1215/dca-prep-kit/tree/master/docs',
+          editUrl: "https://github.com/Piotr1215/dca-prep-kit/tree/master/docs",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
   themeConfig:
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-      ({
-        colorMode: {
-          defaultMode: 'dark',
-        },
-        navbar: {
-          title: 'Cloud Rumble',
-          logo: {
-            alt: 'IT Certifications Guide',
-            src: 'img/logo.png',
-          },
-          items: [
-            {
-              type: 'doc',
-              docId: 'intro',
-              position: 'left',
-              label: 'Docs',
-            },
-            {to: '/blog', label: 'Blog', position: 'left'},
-            {
-              href: 'https://github.com/Piotr1215',
-              label: 'GitHub',
-              position: 'right',
-            },{
-              href: 'https://medium.com/@piotrzan',
-              label: 'Medium',
-              position: 'right',
-            },
-            {
-              href: 'https://killercoda.com/decoder',
-              label: 'Killercoda',
-              position: 'right',
-            },
-            {
-              href: 'https://www.youtube.com/channel/UCkWVN7H3JqGtJ5Pv5bvCrAw',
-              label: 'YouTube',
-              position: 'right',
-            }
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "YVUCIOP9V7",
 
-          ],
+        // Public API key: it is safe to commit it
+        apiKey: "ca28f15cd304fda34283080362ddefc6",
+
+        indexName: "itcertificationsguide",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
+      colorMode: {
+        defaultMode: "dark",
+      },
+      navbar: {
+        title: "Cloud Rumble",
+        logo: {
+          alt: "IT Certifications Guide",
+          src: "img/logo.png",
         },
-        footer: {
-          style: 'dark',
-          links: [
-            {
-              title: 'Docs',
-              items: [
-                {
-                  label: 'Docs',
-                  to: '/docs/intro',
-                },
-              ],
-            },
-            {
-              title: 'Community',
-              items: [
-                {
-                  label: 'Medium Blog',
-                  href: 'https://medium.com/@piotrzan',
-                },
-                {
-                  label: 'Twitter',
-                  href: 'https://twitter.com/Piotr1215',
-                },
-              ],
-            },
-            {
-              title: 'More',
-              items: [
-                {
-                  label: 'Blog',
-                  to: '/blog',
-                },
-                {
-                  label: 'GitHub',
-                  href: 'https://github.com/Piotr1215',
-                },
-                {
-                  label: 'Impressum',
-                  to: '/impressum',
-                }
-              ],
-            },
-          ],
-          copyright: `Built with Docusaurus.`,
-        },
-        prism: {
-          theme: lightCodeTheme,
-          darkTheme: darkCodeTheme,
-          additionalLanguages: ['gherkin'], }, }),
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/Piotr1215",
+            label: "GitHub",
+            position: "right",
+          },
+          {
+            href: "https://medium.com/@piotrzan",
+            label: "Medium",
+            position: "right",
+          },
+          {
+            href: "https://killercoda.com/decoder",
+            label: "Killercoda",
+            position: "right",
+          },
+          {
+            href: "https://www.youtube.com/channel/UCkWVN7H3JqGtJ5Pv5bvCrAw",
+            label: "YouTube",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Docs",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Medium Blog",
+                href: "https://medium.com/@piotrzan",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/Piotr1215",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/Piotr1215",
+              },
+              {
+                label: "Impressum",
+                to: "/impressum",
+              },
+            ],
+          },
+        ],
+        copyright: `Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ["gherkin"],
+      },
+    }),
 };
 
 module.exports = config;
