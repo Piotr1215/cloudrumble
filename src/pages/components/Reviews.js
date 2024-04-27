400291a84ef0c810fb7cb69ce1953fb103d21b83
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2024-04-27 16:24:17 +0100

    enh: from reviews to work

--- src/pages/components/Reviews.js
+++ src/pages/components/Reviews.js
@@ -1,27 +1,29 @@
 import * as React from "react";
 import StarSvg from "../../icons/star.svg";
-import StarEmptySvg from "../../icons/starEmpty.svg";
+import ClapSvg from "../../icons/clap.svg";
 import InfiniteAutoplaySwipe from "../../components/infinityAutoplaySwipe";
 import useScreenSize from "../../hooks/useScreenSize";
+import ThemedImage from "@theme/ThemedImage";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import Link from "@docusaurus/Link";
 
 const data = [
   {
-    name: "Tandy B",
-    date: "Feb 4, 2024 1:00 am",
-    rating: 4,
-    comment:
-      "Quick and efficient service. Joining online was easy and information and confirmation came through straight away.",
-
-    image: "/img/person.png",
+    name: "az-300-prep-kit",
+    date: "Feb 4, 2024",
+    rating: 150,
+    comment: "Exercises and notes preparing for az-300 exam",
+    url: "https://github.com/Piotr1215/az-300-prep-kit",
+    platform: "GitHub",
   },
   {
     name: "Tandy C",
     date: "Feb 4, 2024 1:00 am",
-    rating: 4,
+    rating: 540,
     comment:
       "Quick and efficient service. Joining online was easy and information and confirmation came through straight away.",
-
-    image: "/img/person.png",
+    url: "https://github.com/Piotr1215/az-300-prep-kit",
+    platform: "Medium",
   },
   {
     name: "Tandy D",
@@ -29,8 +31,8 @@ const data = [
     rating: 4,
     comment:
       "Quick and efficient service. Joining online was easy and information and confirmation came through straight away.",
-
-    image: "/img/person.png",
+    url: "https://github.com/Piotr1215/az-300-prep-kit",
+    platform: "GitHub",
   },
   {
     name: "Tandy E",
@@ -38,8 +40,8 @@ const data = [
     rating: 4,
     comment:
       "Quick and efficient service. Joining online was easy and information and confirmation came through straight away.",
-
-    image: "/img/person.png",
+    url: "https://github.com/Piotr1215/az-300-prep-kit",
+    platform: "GitHub",
   },
 ];
 
@@ -48,10 +50,10 @@ function Reviews() {
   return (
     <div className="flex flex-col  items-center space-y-6 mb-20">
       <main className="container lg:px-12 ">
-        <span className="minecustom-badge">Testimonials</span>
-        <h2 className="lg:text-3xl">Clients</h2>
+        <span className="minecustom-badge">Work</span>
+        <h2 className="lg:text-3xl">Popular Repositories & Blogs</h2>
         <p className="text-text-400">
-          More than 100+ happy clients
+          More than 1k stars on GitHub and 100K claps on Medium.
           <br />
         </p>
 
@@ -67,32 +69,52 @@ function Reviews() {
 
 function Card({ item }) {
   return (
-    <div className="max-w-96 border my-6 border-gray-500 rounded-2xl p-4 shadow-lg  dark:shadow-gray-800">
-      <div className="flex flex-row justify-between items-start text-sm space-x-2 mb-2">
-        <div className="flex flex-row items-start space-x-2">
-          <img alt="" src={item.image} className="w-16 rounded-full h-16" />
-          <div className="flex flex-col items-start space-y-[2px]">
-            <p className="font-semibold mb-0">{item.name}</p>
-            <span className="text-xs">{item.date}</span>
-            <Rating rating={item.rating} />
+    <Link
+      href={item.url}
+      style={{
+        textDecoration: "none",
+        color: "inherit",
+      }}
+    >
+      <div className="max-w-96 border min-h-48 my-6 border-gray-500 rounded-2xl p-4 shadow-lg  dark:shadow-gray-800">
+        <div className="flex flex-row justify-between items-start text-sm space-x-2 mb-2">
+          <div className="flex flex-row items-start space-x-2">
+            <ThemedImage
+              sources={{
+                light:
+                  item.platform === "Medium"
+                    ? useBaseUrl("/img/medium.svg")
+                    : "/img/github.svg",
+                dark:
+                  item.platform === "Medium"
+                    ? useBaseUrl("/img/medium-dark.svg")
+                    : "/img/github-dark.svg",
+              }}
+              alt=""
+              className="w-16 rounded-full h-16"
+            />
+            <div className="flex flex-col items-start space-y-[2px]">
+              <p className="font-semibold mb-0">{item.name}</p>
+              <span className="text-xs">{item.date}</span>
+              <Rating platform={item.platform} rating={item.rating} />
+            </div>
           </div>
         </div>
+        <p className="text-sm text-left">&quot;{item.comment}&quot;</p>
       </div>
-      <p className="text-sm text-left">&quot;{item.comment}&quot;</p>
-    </div>
+    </Link>
   );
 }
 
-function Rating({ rating }) {
-  let missingStars = 5 - rating;
+function Rating({ rating, platform }) {
   return (
-    <div className="flex flex-row items-center space-x-[2px]">
-      {Array.from({ length: rating }, (_, i) => (
-        <StarSvg className="w-6 h-6 " key={i} />
-      ))}
-      {Array.from({ length: missingStars }, (_, i) => (
-        <StarEmptySvg className="w-6 h-6 " key={i} />
-      ))}
+    <div className="flex flex-row items-center justify-center text-center space-x-1">
+      <span className="text-sm">{rating}</span>
+      {platform === "Medium" ? (
+        <ClapSvg className="w-6" />
+      ) : (
+        <StarSvg className="w-6 " />
+      )}
     </div>
   );
 }
