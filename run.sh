71ac9e423888d2172597d4811c7c31bcaf59516c
Piotr Zaniewski <piotrzan@gmail.com>
2024-08-31 18:34:11 +0200

    only run local page render when run arg passed to tmuxinator call
    Signed-off-by: Piotr Zaniewski <piotrzan@gmail.com>

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# The set -e option instructs bash to immediately exit if any command has a non-zero exit status
-# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
-# The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
-# https://bit.ly/37nFgin
-set -euo pipefail
-# Set new line and tab for word splitting
-IFS=$'\n\t'
+set -e
 
-# Runs dev loop
-mprocs "npm run start"
+# Runs dev loop if 'run' parameter is passed
+if [ "$1" = "run" ]; then
+	mprocs "npm run start"
+fi
